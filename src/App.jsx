b5b6fa5f3edd5721d13436495c366610a29083e1
Davis Junior <dclimaj@outlook.com>
2025-03-28 09:28:37 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { Icon } from "@iconify/react";
+import AddTaskBox from "./components/AddTaskBox";
+import TaskList from "./components/TaskList";
+import TopBar from "./components/TopBar";
 
 function App() {
-
   return (
     <>
-      <div className="text-xl m-2 p-2 bg-gray-200 rounded">
-        Bootstrap React + vite
-      </div>
-      <div className="text-xl m-2 p-2 border">
-        + WindCss
-      </div>
-      <div className="text-xl m-2 p-2 border">
-        + Iconify <Icon icon="mdi:home" className="text-2xl" />
-      </div>
+      <TopBar />
+      <AddTaskBox />
+      <TaskList />
     </>
   )
 }
